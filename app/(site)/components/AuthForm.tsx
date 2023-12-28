71856429446ef6520bc5f28d21b95663147f1b69
batisuoc <zhenghengyue@gmail.com>
2023-12-28 16:44:52 +0700

    Add 2 more packages & Small update

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,4 +1,22 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
+
+type Variant = "LOGIN" | "REGISTER";
+
 const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
   return <div>Auth Form!</div>;
 };
 
