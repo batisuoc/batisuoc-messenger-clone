4e1db241db9372d1a6333c996f62a604d7e76f5c
batisuoc <zhenghengyue@gmail.com>
2023-12-29 17:44:17 +0700

    Add Input and Button components

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useCallback, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import Input from "@/app/components/inputs/Input";
+import Button from "@/app/components/Button";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -17,7 +19,52 @@ const AuthForm = () => {
     }
   }, [variant]);
 
-  return <div>Auth Form!</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FieldValues>({
+    defaultValues: { name: "", email: "", password: "" }
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (variant === "REGISTER") {
+      // Axios register
+    }
+
+    if (variant === "LOGIN") {
+      // NextAuth SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+    // NextAuth social sign-in
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input id="email" label="Email" register={register} errors={errors} />
+          <Input
+            id="password"
+            label="Password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
